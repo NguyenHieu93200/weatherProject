9407f222233a0ba7a05c4f19cce5bfe386ba10ee
Nguyen Ba Luan <61571929+dongcuavodoi@users.noreply.github.com>
2021-12-23 10:03:30 +0700

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,38 +1,32 @@
 <template>
-  <q-page class="flex column" :class='bgClass'>
-    <div class="col q-pt-lg q-px-md" style="max-height:100px">
+  <q-page class="flex column" :class="bgClass">
+    <div class="col q-pt-lg q-px-md" style="max-height: 100px">
       <q-input
         v-model="search"
         @keyup.enter="getWeatherBySearch"
         label="Search"
         dark
-        borderless>
+        borderless
+      >
         <template v-slot:prepend>
-          <q-icon
-            @click="getLocation"
-            name="my_location" />
+          <q-icon @click="getLocation" name="my_location" />
         </template>
         <template v-slot:append>
-          <q-btn
-            @click="getWeatherBySearch"
-            round
-            dense
-            flat
-            icon="search" />
+          <q-btn @click="getWeatherBySearch" round dense flat icon="search" />
         </template>
       </q-input>
     </div>
 
-    <template v-if = "weatherData">
+    <template v-if="weatherData">
       <div class="row">
         <div class="col-8 column">
           <div class="col-8">
             <div class="col text-white text-center">
               <div class="text-h4 text-weight-light">
-                {{weatherData.name}}
+                {{ weatherData.name }}
               </div>
               <div class="text-h6 text-weight-light">
-                {{weatherData.weather[0].main}}
+                {{ weatherData.weather[0].main }}
               </div>
               <div class="text-h1 text-weight-thin q-my-lq relative-position">
                 <span>{{ Math.round(weatherData.main.temp) }}</span>
@@ -40,10 +34,12 @@
               </div>
             </div>
             <div class="col text-center">
-              <img :src="`http://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`">
+              <img
+                :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`"
+              />
             </div>
           </div>
-          <div class = "col row text-white" style = "overflow: auto" >
+          <!-- <div class = "col row text-white" style = "overflow: auto" >
             <div class="text-h3 text-weight-thin q-my-lq relative-position row" v-for="hour in weatherHourly" :key="hour.dt">
               <div class="col-4"><img width="75" height="75" :src="`http://openweathermap.org/img/wn/${ hour.weather[0].icon }@2x.png`"></div>
               <div class="col">
@@ -54,14 +50,37 @@
                 </div>
               </div>
             </div>
-          </div>
+          </div> -->
+          <q-scroll-area style="height: 230px; max-width: 500px">
+            <div class="row no-wrap">
+            <div style="width: 150px" class="q-pa-sm" v-for="hour in weatherHourly" :key="hour.dt">
+              <div class="col-4"><img width="75" height="75" :src="`http://openweathermap.org/img/wn/${ hour.weather[0].icon }@2x.png`"></div>
+              <div class="col">
+                <div class="row text-h5">{{formatHour(hour.dt)}}</div>
+                <div class="row">
+                  <span>{{ Math.round(hour.temp - 273.15) }}</span>
+                  <span class="text-h6 relative-position">&deg;C</span>
+                </div>
+              </div>
+            </div>
+            </div>
+          </q-scroll-area>
         </div>
         <div class="col-4 text-white">
-          <div class="text-h3 text-weight-thin q-my-lq relative-position row" v-for="day in weatherDailys" :key="day.dt">
-            <div class="col-4"><img width="75" height="75" :src="`http://openweathermap.org/img/wn/${ day.weather[0].icon }@2x.png`"></div>
+          <div
+            class="text-h3 text-weight-thin q-my-lq relative-position row"
+            v-for="day in weatherDailys"
+            :key="day.dt"
+          >
+            <div class="col-4">
+              <img
+                width="75"
+                height="75"
+                :src="`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`"
+              />
+            </div>
             <div class="col">
-              <!-- time -->
-              <div class="row text-h5">{{dayFullName(day.dt)}}</div>
+              <div class="row text-h5">{{ dayFullName(day.dt) }}</div>
               <div class="row">
                 <span>{{ Math.round(day.temp.eve - 273.15) }}</span>
                 <span class="text-h6 relative-position">&deg;C</span>
@@ -75,14 +94,9 @@
 
     <template v-else>
       <div class="col column text-center text-white">
-        <div class="col text-h2 text-weight-thin">
-          Weather<br>App
-        </div>
+        <div class="col text-h2 text-weight-thin">Weather<br />App</div>
       </div>
-      <q-btn
-        @click="getLocation"
-        class="col"
-        flat>
+      <q-btn @click="getLocation" class="col" flat>
         <q-icon left size="3em" name="my_location" />
         <div>Find my location</div>
       </q-btn>
@@ -91,136 +105,143 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { api } from 'boot/axios';
-import moment from 'moment';
+import { defineComponent } from "vue";
+import { api } from "boot/axios";
+import moment from "moment";
 
 export default defineComponent({
-  name: 'PageIndex',
+  name: "PageIndex",
   data() {
     return {
-      search: '',
+      search: "",
       weatherData: null,
       weatherDailys: null,
       weatherHourly: null,
       lat: null,
       lon: null,
-      apiUrl: 'https://api.openweathermap.org/data/2.5/weather?',
-      apiUrl2: 'https://api.openweathermap.org/data/2.5/onecall?',
-      apiKey: '406f101ee5301278cd0a98ef50706a37'
-    }
+      apiUrl: "https://api.openweathermap.org/data/2.5/weather?",
+      apiUrl2: "https://api.openweathermap.org/data/2.5/onecall?",
+      apiKey: "406f101ee5301278cd0a98ef50706a37",
+    };
   },
   computed: {
     bgClass() {
-      if(this.weatherData) {
-        if (this.weatherData.weather[0].icon.endsWith('n')) {
-          return 'bg-night'
+      if (this.weatherData) {
+        if (this.weatherData.weather[0].icon.endsWith("n")) {
+          return "bg-night";
+        } else {
+          return "bg-day";
         }
-        else { 
-          return 'bg-day'
-        }
-      } else return 'bg-day'
+      } else return "bg-day";
     },
   },
   methods: {
     getLocation() {
-      this.$q.loading.show()
+      this.$q.loading.show();
 
       if (this.$q.platform.is.electron) {
-        api.get("https://freegeoip.app/json/")
-          .then(response => {
-            this.lat = response.data.latitude
-            this.lon = response.data.longitude
-            this.getWeatherByCoords()
-          })
-      }
-
-      else{
-        navigator.geolocation.getCurrentPosition(response => {
-          this.lat = response.coords.latitude
-          this.lon = response.coords.longitude
-          this.getWeatherByCoords()
+        api.get("https://freegeoip.app/json/").then((response) => {
+          this.lat = response.data.latitude;
+          this.lon = response.data.longitude;
+          this.getWeatherByCoords();
+        });
+      } else {
+        navigator.geolocation.getCurrentPosition((response) => {
+          this.lat = response.coords.latitude;
+          this.lon = response.coords.longitude;
+          this.getWeatherByCoords();
         });
       }
     },
     async getWeatherByCoords() {
-      this.$q.loading.show()
-      await api.get(`${this.apiUrl}lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`)
-        .then(response => {
+      this.$q.loading.show();
+      await api
+        .get(
+          `${this.apiUrl}lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`
+        )
+        .then((response) => {
           console.log(response.data);
-          this.weatherData = response.data
-        })
+          this.weatherData = response.data;
+        });
 
-      this.getDaily(this.lat,this.lon)
-      this.getHourly(this.lat,this.lon)
-        
+      this.getDaily(this.lat, this.lon);
+      this.getHourly(this.lat, this.lon);
     },
-    getDaily(lat,lon){
-      api.get(`${this.apiUrl2}lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&appid=${this.apiKey}`)
-        .then(response => {
-          this.weatherDailys = response.data.daily
+    getDaily(lat, lon) {
+      api
+        .get(
+          `${this.apiUrl2}lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&appid=${this.apiKey}`
+        )
+        .then((response) => {
+          this.weatherDailys = response.data.daily;
           // console.log(this.lat, this.lon)
-          console.log(this.weatherDaily)
+          console.log(this.weatherDaily);
           // this.$q.loading.hide()
-        })
+        });
     },
-    getHourly(lat,lon){
-      api.get(`${this.apiUrl2}lat=${lat}&lon=${lon}&exclude=current,minutely,daily&appid=${this.apiKey}`)
-        .then(response => {
-          this.weatherHourly = response.data.hourly
-          this.weatherHourly.splice(12,36)
-          this.$q.loading.hide()
-        })
+    getHourly(lat, lon) {
+      api
+        .get(
+          `${this.apiUrl2}lat=${lat}&lon=${lon}&exclude=current,minutely,daily&appid=${this.apiKey}`
+        )
+        .then((response) => {
+          this.weatherHourly = response.data.hourly;
+          this.weatherHourly.splice(12, 36);
+          this.$q.loading.hide();
+        });
     },
     async getWeatherBySearch() {
-      let latitude = 0
-      let longitude = 0
-      this.$q.loading.show()
-      await api.get(`${this.apiUrl}q=${ this.search }&appid=${this.apiKey}&units=metric`)
-        .then(response => {
-          this.weatherData = response.data
+      let latitude = 0;
+      let longitude = 0;
+      this.$q.loading.show();
+      await api
+        .get(`${this.apiUrl}q=${this.search}&appid=${this.apiKey}&units=metric`)
+        .then((response) => {
+          this.weatherData = response.data;
           // this.$q.loading.hide()
-          latitude = response.data.coord.lat
-          longitude = response.data.coord.lon
-        })
-      this.getDaily(latitude,longitude)
-      this.getHourly(latitude,longitude)
+          latitude = response.data.coord.lat;
+          longitude = response.data.coord.lon;
+        });
+      this.getDaily(latitude, longitude);
+      this.getHourly(latitude, longitude);
     },
-    weekDate (value) {
-    const entireWeek = moment(value).format("MMMM D -") + moment(value).add(7, 'days').format("D YYYY") 
-    return entireWeek; // July 6 - 13 2020
+    weekDate(value) {
+      const entireWeek =
+        moment(value).format("MMMM D -") +
+        moment(value).add(7, "days").format("D YYYY");
+      return entireWeek; // July 6 - 13 2020
     },
-    dayFullName (date) {
-      let myDate = new Date(date * 1000)
-      let myDay = moment(myDate, 'x').format('dddd DD/MM')
-      return myDay
+    dayFullName(date) {
+      let myDate = new Date(date * 1000);
+      let myDay = moment(myDate, "x").format("dddd DD/MM");
+      return myDay;
     },
-    format_date(value){
+    format_date(value) {
       if (value) {
-          return moment(String(value)).format('MM/DD/YYYY hh:mm')
+        return moment(String(value)).format("MM/DD/YYYY hh:mm");
       }
     },
-    formatHour(date){
-      let myDate = new Date(date * 1000)
-      let myDay = moment(myDate, 'x').format('hh:mm A DD/MM')
-      return myDay
-    }
-  }
-})
+    formatHour(date) {
+      let myDate = new Date(date * 1000);
+      let myDay = moment(myDate, "x").format("hh:mm A DD/MM");
+      return myDay;
+    },
+  },
+});
 </script>
 
 <style lang="sass">
-  .q-page
-    background: linear-gradient(to bottom,#136a8a,#267871)
-    &.bg-night
-      background: linear-gradient(to bottom,#232526, #414345)
-    &.bg-day
-      background: linear-gradient(to bottom,#00b4db, #0083b0)  
-  .degree
-    top: -50px
-  .skyline
-    flex: 0 0 100px
-    background: url(/picture.png)
-    background-size: contain
-    background-position: center bottom
+.q-page
+  background: linear-gradient(to bottom,#136a8a,#267871)
+  &.bg-night
+    background: linear-gradient(to bottom,#232526, #414345)
+  &.bg-day
+    background: linear-gradient(to bottom,#00b4db, #0083b0)
+.degree
+  top: -50px
+.skyline
+  flex: 0 0 100px
+  background: url(/picture.png)
+  background-size: contain
+  background-position: center bottom
 </style>
