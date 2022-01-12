39432a0f8a06213ef57170083feed0311c5d6406
NguyenHieu93200 <91836161+NguyenHieu93200@users.noreply.github.com>
2022-01-12 21:40:48 +0700

    push

---
+++ src/components/mapPick.vue
@@ -0,0 +1,35 @@
+<template>
+  <GoogleMap
+  api-key="AIzaSyClvH-MxHTMWVOVxND6wHPkJkWCGjYMZIA"
+  style="width: 100%; height: 500px"
+  :center="center"
+  :zoom="13"
+  >
+  </GoogleMap>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { GoogleMap } from 'vue3-google-map'
+export default defineComponent({
+  name: 'mapPick',
+  components: { GoogleMap},
+   props: {
+     lat: String,
+     lng: String,
+   },
+  data() {
+    var lat = parseFloat(this.lat);
+    var lng = parseFloat(this.lng);
+    return { 
+        center: { lat: lat, lng: lng }, 
+    }
+  },
+  watch: {
+      lat: function(newVal, oldVal) {
+          if(newVal != oldVal)
+            this.$forceUpdate();
+      }
+  }
+})
+</script>
